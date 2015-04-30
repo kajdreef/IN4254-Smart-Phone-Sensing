24cf30992318951ca1da2eb7e55c7d27a366af51
Uncle John <d.derksen@isae.fr>
2015-04-30 16:24:11 +0200

    Skeleton of testClassification

---
+++ app/src/main/java/io/github/kajdreef/smartphonesensing/Classification/TestClassification/testClassification.java
@@ -0,0 +1,21 @@
+package io.github.kajdreef.smartphonesensing.Classification.TestClassification;
+
+import android.test.ActivityTestCase;
+
+public class testClassification extends ActivityTestCase {
+    @Override
+    protected void setUp() throws Exception  {
+        //Get data from files as 3 arraylists for 1 training point (15-20 points?)
+
+
+        //Extract feature
+        //Add labels
+        //Train KNN
+        //Test unlabeled data point
+
+
+    }
+    public void testClassification(){
+
+    }
+}
