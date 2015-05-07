d6954658280b9fa6f75855890aea00be38a23e2c
Uncle John <d.derksen@isae.fr>
2015-05-07 16:15:08 +0200

    FeatureExtractorMag

---
+++ app/src/main/java/io/github/kajdreef/smartphonesensing/Classification/FeatureExtractorMag.java
@@ -0,0 +1,18 @@
+package io.github.kajdreef.smartphonesensing.Classification;
+
+import java.util.ArrayList;
+
+import io.github.kajdreef.smartphonesensing.Utils.ArrayOperations;
+
+public class FeatureExtractorMag extends FeatureExtractor {
+    @Override
+    public FeatureSet extractFeatures(ArrayList<Float> x, ArrayList<Float> y, ArrayList<Float> z) {
+        ArrayList<Float> magnitude = new ArrayList<>(x.size());
+
+        for (int i = 0;i<x.size();i++){
+            magnitude.add(i, (float)(Math.sqrt(Math.pow(x.get(i),2.0) + Math.pow(y.get(i),2.0) + Math.pow(z.get(i),2.0))));
+        }
+        return new FeatureSet(ArrayOperations.maximumValue(magnitude)/x.size());
+    }
+
+}
